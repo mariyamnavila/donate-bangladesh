5cd22077589ef48b2b7973a3d60b30966420803e
mariyamnavila <bibimariyamnavila@gmail.com>
2025-07-13 20:12:17 +0600

    fixed

--- home.js
+++ home.js
@@ -1,9 +1,9 @@
 function btnCall(id) {
         if (id === 'blog-btn') {
-            window.location.href = 'donate-bangladesh/home.html';
+            window.location.href = '/home.html';
         }
         else if (id === 'home-btn') {
-            window.location.href = '/index.html';
+            window.location.href = 'donate-bangladesh/index.html';
         }
     }
 // document.getElementById('home-btn')
