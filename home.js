d8a91921519840188dfc3ebcbd0cbb187ea62728
mariyamnavila <bibimariyamnavila@gmail.com>
2025-07-13 14:22:15 +0600

    added home page

---
+++ home.js
@@ -0,0 +1,12 @@
+function btnCall(id) {
+        if (id === 'blog-btn') {
+            window.location.href = '/home.html';
+        }
+        else if (id === 'home-btn') {
+            window.location.href = '/blog.html';
+        }
+    }
+// document.getElementById('home-btn')
+//     .addEventListener('click', function(){
+//         window.location.href = '/blog.html';
+// });
